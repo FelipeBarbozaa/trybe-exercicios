347ac57468323738380a30340688ba44c78938c2
Felipe Barboza <felipebarboza5@gmail.com>
2021-10-06 18:07:48 -0300

    exercicios.js atualizado

--- modulo1-fundamentos/b4-javascript/d2-array-e-loop-for/exercicios.js
+++ modulo1-fundamentos/b4-javascript/d2-array-e-loop-for/exercicios.js
@@ -17,6 +17,7 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media = 0;
 
 
 //1)  Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
@@ -31,3 +32,38 @@ for (let numeros of numbers){
 console.log(soma)
 
 //3) Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+media = soma/numbers.length
+console.log(media)
+
+//4) Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (media > 20){
+    console.log("Valor maior que 20")
+} else {
+    console.log ('Valor menor ou igual a 20')
+}
+
+//5) Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let maior = 0;
+for (let numeros of numbers){
+    if (numeros > maior){
+        maior = numeros
+    }
+}
+console.log(maior)
+
+//6) Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let impar = []
+for (let numeros of numbers){
+    if (numeros % 2 == 1){
+        impar.push(numeros)
+    }
+}
+
+if (impar.length <= 0){
+    console.log('Nenhum valor impar encontrado')
+}
+console.log(impar)
