6c10b6eacc04d8e70fa3a5b781a73fe572966db2
Felipe Barboza <felipebarboza5@gmail.com>
2021-10-19 15:13:00 -0300

    exercicios finalizado

---
+++ modulo1-fundamentos/b5-dom-seletores/b5-dom-eventos-webstorage/script.js
@@ -0,0 +1,20 @@
+for(let i = 0; i<2; i+=1){
+  document.querySelectorAll("section.emergency-tasks h3")[i].style="background-color: #A500F3"
+  document.querySelectorAll("section.emergency-tasks div")[i].style="background-color: #FF9F84"
+  document.querySelectorAll('section.no-emergency-tasks h3')[i].style="background-color: #232525"
+  document.querySelectorAll('section.no-emergency-tasks div')[i].style='background-color: #F9DB5E'
+
+}
+
+document.querySelectorAll("section.emergency-tasks")[0].style="background-color: #FF9F84"
+document.querySelectorAll('section.no-emergency-tasks')[0].style='background-color: #F9DB5E'
+document.getElementsByTagName('h1')[0].style = "background-color: #00B069"
+document.getElementsByTagName('footer')[0].style = "background-color: #003533"
+
+
+
+
+
+
+
+
