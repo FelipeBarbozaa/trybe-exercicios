d858bf364e0ecdc1cefddfe71950da8a1c4087e4
Felipe Barboza <felipebarboza5@gmail.com>
2021-10-21 18:09:51 -0300

    exercicio 1 e 2

--- modulo1-fundamentos/b5-dom-seletores/d3-eventos/exercicios/script.js
+++ modulo1-fundamentos/b5-dom-seletores/d3-eventos/exercicios/script.js
@@ -13,4 +13,52 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const sexta = [5, 12, 19, 26]
+const feriado = [25, 26, 32]
+
+function calendario() {
+  for (let dias of dezDaysList) {
+    document.querySelector("#days").appendChild(document.createElement('li')).innerText = dias
+  }
+
+  for (let dias in dezDaysList) {
+    document.querySelectorAll("#days li")[dias].className = "day"
+  }
+
+  for (let dias of feriado) {
+    document.querySelector("#days").children[dias].className += " holiday"
+  }
+
+  for (let dias of sexta) {
+    document.querySelector("#days").children[dias].className += " friday"
+  }
+
+}
+calendario(), holiday()
+
+function holiday(Feriados) {
+  document.querySelector(".buttons-container").appendChild(document.createElement("button"))
+  document.querySelector(".buttons-container").children[0].id = "btn-holiday"
+}
+
+
+document.querySelector("#btn-holiday").addEventListener('click', mudarCor)
+
+let condicao = 0
+function mudarCor() { 
+  let array = document.querySelectorAll(".holiday")
+  if (condicao %2 === 0) {
+    for (index in array) {
+      document.querySelectorAll(".holiday")[index].style = "background-color: green"
+    }
+  }
+  if (condicao %2 === 1) {
+    for (index in array) {
+      document.querySelectorAll(".holiday")[index].style = "background-color: rgb(238,238,238)"
+    }
+  }
+  condicao += 1
+}
+
+
