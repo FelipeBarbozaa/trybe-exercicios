983d9700228cdc1ca082db5ee8d1c551044d1174
Felipe Barboza <felipebarboza5@gmail.com>
2021-10-28 12:16:31 -0300

    exercicios

---
+++ modulo1-fundamentos/b6-html-css-forms-flexbox-responsivo/d1-forms/exercicios/script.js
@@ -0,0 +1,42 @@
+const states = [
+  { value: 'AC', label: 'Acre' },
+  { value: 'AL', label: 'Alagoas' },
+  { value: 'AP', label: 'Amapá' },
+  { value: 'AM', label: 'Amazonas' },
+  { value: 'BA', label: 'Bahia' },
+  { value: 'CE', label: 'Ceará' },
+  { value: 'DF', label: 'Distrito Federal' },
+  { value: 'ES', label: 'Espírito Santo' },
+  { value: 'GO', label: 'Goías' },
+  { value: 'MA', label: 'Maranhão' },
+  { value: 'MT', label: 'Mato Grosso' },
+  { value: 'MS', label: 'Mato Grosso do Sul' },
+  { value: 'MG', label: 'Minas Gerais' },
+  { value: 'PA', label: 'Pará' },
+  { value: 'PB', label: 'Paraíba' },
+  { value: 'PR', label: 'Paraná' },
+  { value: 'PE', label: 'Pernambuco' },
+  { value: 'PI', label: 'Piauí' },
+  { value: 'RJ', label: 'Rio de Janeiro' },
+  { value: 'RN', label: 'Rio Grande do Norte' },
+  { value: 'RS', label: 'Rio Grande do Sul' },
+  { value: 'RO', label: 'Rondônia' },
+  { value: 'RR', label: 'Roraíma' },
+  { value: 'SC', label: 'Santa Catarina' },
+  { value: 'SP', label: 'São Paulo' },
+  { value: 'SE', label: 'Sergipe' },
+  { value: 'TO', label: 'Tocantins' },
+  ];
+
+  window.onload = () => {
+    for (let index = 0; index < states.length; index += 1) {
+      document.querySelector('#listaDeEstados').appendChild(document.createElement('option'));
+      let opcao = document.querySelectorAll('#listaDeEstados option');
+      opcao[index].innerText = states[index].label;
+      opcao[index].value = states[index].value;
+    } 
+  };
+
+  document.querySelector('#enviar').addEventListener('click', (value) => {
+    value.preventDefault()
+})
