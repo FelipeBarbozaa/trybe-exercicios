30031a41dce9afc25c3d7abbd8f1bb75272a0446
Felipe Barboza <felipebarboza5@gmail.com>
2021-11-12 17:54:50 -0300

    Aprendendo

---
+++ modulo1-fundamentos/b7-es6-testes-unitarios/d3-jest/sum.test.js
@@ -0,0 +1,16 @@
+const sum = require('./sum');
+
+describe('Requisito 1 - Função de soma', () => {
+  it('A função recebe 2 e 3 e retorna 5', () => {
+    expect(5).toEqual(sum(2, 3))
+  })
+
+  it('A função recebe 5 e 5 e retorna 10', () => {
+    expect(10).toEqual(sum(5, 5))
+  })
+})
+
+
+test('A função recebe 5 e 4 e retorna 9', () => {
+  expect(sum(5, 4)).toBe(9)
+})
