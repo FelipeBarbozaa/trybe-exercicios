067a165b4b4aaafba93aacc32bba73e59b566067
Felipe Barboza <felipebarboza5@gmail.com>
2021-11-17 16:05:52 -0300

    Exercicio 3

--- modulo1-fundamentos/b8-higher-order-functions-es6/d1-introducao-higher-order-functions/script.js
+++ modulo1-fundamentos/b8-higher-order-functions-es6/d1-introducao-higher-order-functions/script.js
@@ -1,3 +1,5 @@
+console.log('=====')
+console.log('Exercicio 1')
 const cadastro = (nome) => {
   const dados = {
     nome: nome,
@@ -15,15 +17,12 @@ const newEmployees = () => {
   return employees;
 };
 
-
-
-
-
 const sorteio = (numero, func) => {
   const numeroSorteado = Math.ceil(Math.random()*5)
   func(numero, numeroSorteado)
 }
-
+console.log('=====')
+console.log('Exercicio 2')
 const comparar = (numero, numeroSorteado) => {
   if (numero === numeroSorteado) {
     console.log(`Parabéns, você acertou!!
@@ -35,5 +34,29 @@ Numero escolhido: ${numero}
 Numero sorteado: ${numeroSorteado}`)
   }
 }
-
 sorteio(3, comparar)
+
+console.log('=====')
+console.log('Exercicio 3')
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const hof = (gabarito, resposta, func) => {
+  func(gabarito, resposta)
+}
+
+const verificar = (gabarito, resposta) => {
+  let pontos = 0
+  for (let i = 0; i < resposta.length; i += 1) {
+    if (resposta[i] === 'N.A') {
+      pontos += 0
+    } else if (resposta [i] !== gabarito[i]) {
+      pontos -= 0.5
+    } else {
+      pontos += 1
+    }
+  }
+  console.log(pontos)
+}
+
+hof(RIGHT_ANSWERS, STUDENT_ANSWERS, verificar)
