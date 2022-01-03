2e91d7bc99a128a7cee22daa3e1d25b118823c6f
Felipe Barboza <felipebarboza5@gmail.com>
2022-01-03 18:21:52 -0300

    Aprendendo React

---
+++ modulo2-frontend/b1-introducao-react/d1-hello-world-react/primeiro_app/src/Component.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class SobreMim extends React.Component {
+    render() {
+        return <h1>Felipe, <p>Gosto de estudar programação e quero ter uma carreira de sucesso!</p> </h1>
+    }
+}
