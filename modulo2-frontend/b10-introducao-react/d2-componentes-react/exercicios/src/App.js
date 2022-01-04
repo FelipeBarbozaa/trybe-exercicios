293ed55769f3b8f6d6d5f3059765b1bfcdb3b374
Felipe Barboza <felipebarboza5@gmail.com>
2022-01-04 12:53:45 -0300

    Exercicios

---
+++ modulo2-frontend/b10-introducao-react/d2-componentes-react/exercicios/src/App.js
@@ -0,0 +1,14 @@
+import Image from './Image';
+import './App.css';
+
+const staringCat = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
+
+function App() {
+  return (
+    <section>
+      <Image source={staringCat} alternativeText='Cute cat staring'/>
+    </section>
+  );
+}
+
+export default App;
