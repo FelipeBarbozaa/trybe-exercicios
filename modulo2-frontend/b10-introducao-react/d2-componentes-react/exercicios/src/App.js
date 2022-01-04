bba621d7f4719fe117036f131117229578d3495e
Felipe Barboza <felipebarboza5@gmail.com>
2022-01-04 13:31:55 -0300

    Exercicios

--- modulo2-frontend/b10-introducao-react/d2-componentes-react/exercicios/src/App.js
+++ modulo2-frontend/b10-introducao-react/d2-componentes-react/exercicios/src/App.js
@@ -1,14 +1,38 @@
-import Image from './Image';
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
 import './App.css';
+import Order from './Order';
 
-const staringCat = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
 
-function App() {
-  return (
-    <section>
-      <Image source={staringCat} alternativeText='Cute cat staring'/>
-    </section>
-  );
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+         {<Order order={headphone}/>}
+         {<Order order={energyDrink}/>}
+      </div>
+    );
+  }
 }
 
 export default App;
