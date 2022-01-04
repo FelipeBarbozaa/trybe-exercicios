7c768af1771353d481242c55205c732b84a93523
Felipe Barboza <felipebarboza5@gmail.com>
2022-01-03 21:37:31 -0300

    Aprendendo React

---
+++ modulo2-frontend/b1-introducao-react/d1-hello-world-react/exercicios_2/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className='footer'>
+         <h1>E isso é só o começo...</h1>
+      </foote  r>
+    )
+  }
+}
+
+export default Footer
