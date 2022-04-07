246157bf8a77bd92aeb25bb9c204cb5023752587
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-07 14:32:59 -0300

    Atualizando

--- modulo3-backend/b22-node/my-scripts/imc.js
+++ modulo3-backend/b22-node/my-scripts/imc.js
@@ -1,7 +1,20 @@
 const readline = require('readline-sync');
 
 const peso = readline.questionInt('Qual seu peso ? : ')
-const altura = 1.65
-const imc = peso / (altura ** 2)
+const altura = readline.questionInt('Qual sua altura (cm) ? : ')
+const imc = peso / (( altura / 100 ) ** 2)
+console.log(imc.toFixed(2))
 
-console.log(imc.toFixed(2));
+if (imc < 18.5) {
+  console.log('Abaixo do peso (magreza)')
+} else if (imc <= 24.9) {
+  console.log('Peso normal')
+} else if (imc <= 29.9) {
+  console.log('Acima do peso (sobrepeso)')
+} else if (imc <= 34.9) {
+  console.log('Obesidade grau I')
+} else if (imc <= 39.9) {
+  console.log('Obesidade grau II')
+} else {
+  console.log('Obesidade grau III e IV')
+}
