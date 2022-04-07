2a1f6d5cce2802eddf6162f615d6323be5521281
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-07 20:40:36 -0300

    Atualizando

--- modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex001.js
+++ modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex001.js
@@ -1,4 +1,4 @@
-function exercicio(num1, num2, num3) {
+const exercicio = (num1, num2, num3) => {
   return new Promise((resolve, reject) => {
     if ((num1 + num2) * num3 < 50) return new Error(reject('Valor muito baixo!'))
     if(typeof(num1) !== 'number' || typeof(num2) !== 'number' || typeof(num3) !== 'number') {
@@ -8,13 +8,6 @@ function exercicio(num1, num2, num3) {
   })
 }
 
-exercicio(10, 10, 8).then((resultado) => {
-  console.log(`Resultado: ${resultado}`)
-})
-.catch((err) => console.log(`Erro: ${err}`));
-
-
-function exercicio2(num1, num2, num3) {
-  return (num1 + num2) * num3
-}
-
+module.exports = {
+  exercicio
+};
---
+++ modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex002.js
@@ -0,0 +1,8 @@
+const exercicio = require('./ex001');
+
+const randomNumber = () => {
+  num1 = Math.round(Math.random() * 100 );
+  console.log(num1);
+};
+
+randomNumber();
