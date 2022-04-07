8a43bc02bda1cafc0921ea9d76b4a5fe5313761b
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-07 14:35:58 -0300

    Atualizando

---
+++ modulo3-backend/b22-node/my-scripts/velocidade.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+
+const distancia = readline.questionInt('Qual a distância?(metros) : ');
+const tempo = readline.questionInt('Qual é o tempo (segundos)? : ');
+
+const velocidade = distancia / tempo;
+
+console.log(velocidade);
