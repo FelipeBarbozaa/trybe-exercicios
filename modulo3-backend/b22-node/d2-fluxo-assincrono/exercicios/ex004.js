0da96b026fca6a06386a0d309eb641cd8fcf10ee
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-07 22:13:52 -0300

    Atualizando

--- modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex004.js
+++ modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex004.js
@@ -47,3 +47,33 @@ const exercicio43 = async () => {
   }
 }
 
+const exercicio44 = async () => {
+  const arquivoJson = await fs.readFile(arquivo, 'utf8')
+  .then((result) => JSON.parse(result));
+  
+  const novoArquivo = arquivoJson.filter((e) => parseInt(e.id) <= 4)
+  novoArquivo.push({ "id": "8", "name": "Nelson Muntz"});
+  try {
+    fs.writeFile('./simpsonFamily.json', JSON.stringify(novoArquivo))
+    console.log('Arquivo escrito com sucesso!')
+  } catch (e) {
+    console.log(`Erro: ${e}`)
+  }
+}
+
+const exercicio45 = () => {
+  // Realizamos a leitura do arquivo
+  return fs.readFile('./simpsonFamily.json', 'utf-8')
+    // Interpretamos o conteúdo como JSON
+    .then((fileContent) => JSON.parse(fileContent))
+    // Filtramos o array para remover o personagem Nelson
+    .then((simpsons) => simpsons.filter((simpson) => simpson.id !== '8'))
+    // Criamos um novo Array contendo a personagem Maggie
+    .then((simpsonsWithoutNelson) => simpsonsWithoutNelson
+      .concat([{ id: '8', name: 'Maggie Simpson' }]))
+    // Escrevemos o novo array no arquivo
+    .then((simpsonsWithMaggie) =>
+      fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsWithMaggie)));
+}
+
+exercicio45();
