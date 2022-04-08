3e8b8fbd8b22da15c40a30fcf30a90720a20cfbd
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-07 21:56:52 -0300

    Atualizando

--- modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex004.js
+++ modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex004.js
@@ -1,3 +1,5 @@
+const { exercicio } = require('./ex001');
+
 const fs = require('fs').promises;
 
 const arquivo = './simpsons.json'
@@ -20,6 +22,16 @@ const exercicio41 = async (findId) => {
   return findCharacter;
 }
 
-const exercicio42 = () => {
-
+const exercicio42 = async () => {
+  const arquivoJson = await fs.readFile(arquivo, 'utf8')
+  .then((result) => JSON.parse(result));
+  
+  const novoArquivo = arquivoJson.filter((e) => e.id !== '10' && e.id !== '6')
+  try {
+    fs.writeFile(arquivo, JSON.stringify(novoArquivo))
+  } catch (e) {
+    console.log(e)
+  }
 }
+
+exercicio42()
