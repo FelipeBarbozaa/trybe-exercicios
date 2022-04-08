f71cf5ba4aa9e5745fd1a92773753cbf9f5ad179
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-08 12:01:45 -0300

    Atualizando

--- modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex005.js
+++ modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex005.js
@@ -1,22 +1,30 @@
+// Crie uma função que lê e escreve vários arquivos ao mesmo tempo.
+
 const fs = require('fs').promises;
 
-Promise.all([
-  fs.writeFile('./arquivo1.txt', 'Finalmente '),
-  fs.writeFile('./arquivo2.txt', 'Estou '),
-  fs.writeFile('./arquivo3.txt', 'Usando '),
-  fs.writeFile('./arquivo4.txt', 'Promise all'),
-])
-  .then(() => {
-    Promise.all([
-      fs.readFile('./arquivo1.txt', 'utf8'),
-      fs.readFile('./arquivo2.txt', 'utf8'),
-      fs.readFile('./arquivo3.txt', 'utf8'),
-      fs.readFile('./arquivo4.txt', 'utf8'),
-    ])
-    .then(([file1, file2, file3, file4]) => {
-      const allFiles = (file1 + file2 + file3 + file4)
-      fs.writeFile('./allFiles.txt', allFiles);
-      console.log('Script executado com sucesso!')
-    })
-  })
-  .catch(error => console.log(error))
+async function arrayToFile() {
+  const strings = ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'];
+
+  const createFilePromises = strings.map((string, index) => {
+    fs.writeFile(`./file${index + 1}.txt`, string)
+  });
+  
+  const fileNames = [
+    'file1.txt',
+    'file2.txt',
+    'file3.txt',
+    'file4.txt',
+    'file5.txt',
+  ];
+
+  const fileContents = await Promise.all(
+    fileNames.map((fileName) => fs.readFile(fileName, 'utf8'))
+  );
+
+  const allFiles = fileContents.join(' ');
+  await fs.writeFile('./allFiles.txt', allFiles)
+}
+
+arrayToFile(
+
+)
