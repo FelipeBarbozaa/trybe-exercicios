a0f75e1112218eaa47099fae6bca183cd2e89387
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-08 12:05:31 -0300

    Atualizando

---
+++ modulo3-backend/b22-node/d2-fluxo-assincrono/exercicios/ex006.js
@@ -0,0 +1,12 @@
+// Crie um script que mostre na tela o conteúdo de um arquivo escolhido pela pessoa usuária:
+
+const fs = require('fs').promises;
+const readline = require('readline-sync');
+
+async function choseFile() {
+  const file = readline.question('Escreva o nome do arquivo que deseja ler: ');
+  const fileRead = await fs.readFile(file, 'utf8');
+  console.log(fileRead);
+}
+
+choseFile();
