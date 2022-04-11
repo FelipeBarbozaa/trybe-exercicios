24a29bb3d7e2b0868691def5b8d0abfd1a54c458
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-11 18:05:11 -0300

    Atualizando

--- modulo3-backend/b22-node/d4-HTTP-nodejs/querystring/index.js
+++ modulo3-backend/b22-node/d4-HTTP-nodejs/querystring/index.js
@@ -8,6 +8,15 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
 app.get('/recipes/search', (req, res) => {
   const { name, maxPrice, minPrice } = req.query;
   const filteredRecipes = recipes.filter((r) => r.name.includes(name)
@@ -16,6 +25,12 @@ app.get('/recipes/search', (req, res) => {
   );
   res.status(200).json(filteredRecipes);
 });
+
+app.get('/drinks/search', (req, res) => {
+  const { name } = req.query;
+  const filteredDrinks = drinks.filter((d) => d.name.includes(name));
+  res.status(200).json(filteredDrinks);
+})
 
 app.listen(3000, () => {
   console.log('Aplicação ouvindo na porta 3000')
