8755ba4b4bea9083ba22932e3ffa8d77250e034d
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-11 22:13:22 -0300

    Atualizando

--- modulo3-backend/b22-node/d4-HTTP-nodejs/recebendoDadosPeloBody/index.js
+++ modulo3-backend/b22-node/d4-HTTP-nodejs/recebendoDadosPeloBody/index.js
@@ -10,6 +10,12 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
  ];
 
+ app.get('/validateToken', (req, res) => {
+   const token = req.headers.authorization;
+   if (token.length !== 16) return res.status(401).json({message: 'Invalid token!'});
+   res.status(200).json({message: 'Valid token!'})
+ })
+
  app.get('/recipes', (req, res) => {
    res.status(200).json(recipes);
  })
