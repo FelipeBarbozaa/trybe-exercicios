6aa5c330f603695f0c144f240a4ac7a8094f318f
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-15 12:35:26 -0300

    Atualizando

--- modulo3-backend/b23-arquitetura-de-software/d1-camada-model/exercicios/index.js
+++ modulo3-backend/b23-arquitetura-de-software/d1-camada-model/exercicios/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 app.use(bodyParser.json());
-const port = 3002
+const port = 3000
 
 const user = require('./models/user');
 
@@ -12,8 +12,16 @@ app.post('/user', async (req, res) => {
   if (!lastName || firstName === '') return res.status(401).json({ message: 'lastName não pode ser vázio! '});
   if (!email.includes('@' && '.com')) return res.status(401).json({ message: 'email inválido!'});
   if (!password) return res.status(401).json({ message: 'Precisa de uma senha!'});
-  if (password.length < 7 || typeof password !== 'string') res.status(401).json({ message: 'Senha inválida!'});
-  if (user.isValid()) res.status(401).json({ messagE: 'Email já existe! '});
+  if (password.length < 7) res.status(401).json({ message: 'Senha inválida!'});
+  if (await user.isValid(email)) return res.status(401).json({ message: 'Email já existe! '});
+  await user.createUser(firstName, lastName, email, password);
+  return res.status(200).json({ message: 'Usuário criado com sucesso!' });
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/user/:id', async (req, res) => {
+  const { id } = req.params;
+  const getUser = await user.getUserById(id);
+  res.status(200).json(getUser);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- modulo3-backend/b23-arquitetura-de-software/d1-camada-model/exercicios/models/user.js
+++ modulo3-backend/b23-arquitetura-de-software/d1-camada-model/exercicios/models/user.js
@@ -1,22 +1,31 @@
 const connection = require('./connection');
+const md5 = require('md5')
 
 const getEmail = async () => {
   const [uniques] = await connection.execute('SELECT email FROM authors');
   return uniques;
 };
 
+const getUserById = async (id) => {
+  const [user] = await connection.execute('SELECT * FROM authors;');
+  const findUser = user.find((e) => e.id === parseInt(id));
+  return findUser;
+}
+
 const isValid = async (email) => {
   const result = await getEmail();
   const checkEmail = result.find((e) => e.email === email)
-  if (checkEmail) return true;
-  return false;
+  if (checkEmail === undefined) return false;
+  return true;
 }
 
-const createUser = async () => {
-  
+const createUser = async (firstName, lastName, email, password) => {
+  await connection.execute('INSERT INTO model_example.authors (first_name, last_name, email, password) VALUES (?, ?, ?, ?)', [firstName, lastName, email, md5(password)]);
 }
 
 module.exports = {
   getEmail,
   isValid,
+  createUser,
+  getUserById,
 }
