523833ff46000582e73ba50b39df7ec9341325b4
Felipe Barboza <felipebarboza5@gmail.com>
2022-04-14 23:09:15 -0300

    Atualizando

--- modulo3-backend/b23-arquitetura-de-software/d1-camada-model/exercicios/index.js
+++ modulo3-backend/b23-arquitetura-de-software/d1-camada-model/exercicios/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const app = express()
 app.use(bodyParser.json());
-const port = 3000
+const port = 3002
 
-app.post('/user', (req, res) => {
-  
+const user = require('./models/user');
+
+app.post('/user', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  if (!firstName || firstName === '') return res.status(401).json({ message: 'firstName não pode ser vázio! '});
+  if (!lastName || firstName === '') return res.status(401).json({ message: 'lastName não pode ser vázio! '});
+  if (!email.includes('@' && '.com')) return res.status(401).json({ message: 'email inválido!'});
+  if (!password) return res.status(401).json({ message: 'Precisa de uma senha!'});
+  if (password.length < 7 || typeof password !== 'string') res.status(401).json({ message: 'Senha inválida!'});
+  if (user.isValid()) res.status(401).json({ messagE: 'Email já existe! '});
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- modulo3-backend/b23-arquitetura-de-software/d1-camada-model/exercicios/models/user.js
+++ modulo3-backend/b23-arquitetura-de-software/d1-camada-model/exercicios/models/user.js
@@ -1,5 +1,22 @@
 const connection = require('./connection');
 
+const getEmail = async () => {
+  const [uniques] = await connection.execute('SELECT email FROM authors');
+  return uniques;
+};
+
+const isValid = async (email) => {
+  const result = await getEmail();
+  const checkEmail = result.find((e) => e.email === email)
+  if (checkEmail) return true;
+  return false;
+}
+
 const createUser = async () => {
   
+}
+
+module.exports = {
+  getEmail,
+  isValid,
 }
