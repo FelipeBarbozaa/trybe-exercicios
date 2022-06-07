070af816794527a7774073057b37a87c3e540f66
Felipe Barboza <felipebarboza5@gmail.com>
2022-06-07 11:44:58 -0300

    Exercicio

---
+++ modulo3-backend/b26-typescript/d2-tipagem-estatica-e-generics/exercicio.ts
@@ -0,0 +1,43 @@
+type Point = {
+  x: number;
+  y: number;
+};
+
+function printCoord(cord: Point) {
+  console.log("O valor da cordenada x é: " + cord.x);
+  console.log("O valor da coordenada y é: " + cord.y);
+}
+
+printCoord({ x: 100, y: 100 });
+
+type Bird = {
+  name: string,
+  age: number,
+  sex: string
+};
+
+function printBird(info: Bird) {
+  console.log(`O nome é: ${info.name}, tem ${info.age} anos e é do sexo ${info.sex}`)
+}
+
+printBird({ name: 'Jubileu', age: 1, sex: 'Não sabemos' })
+
+type Soma = {
+  numberOne: number,
+  numberTwo: number,
+}
+
+function somaDoisNumeros(resultado: Soma) {
+  console.log(`O resultado é ${resultado.numberOne + resultado.numberTwo}`)
+}
+
+somaDoisNumeros({ numberOne: 10, numberTwo: 30})
+
+type address = {
+  pais: string,
+  cidade: string,
+  bairro: string,
+  rua: string,
+  numero: number,
+};
+
