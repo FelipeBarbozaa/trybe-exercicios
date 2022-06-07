f50e4fa374138c455c14b1bb1e6551613f81e761
Felipe Barboza <felipebarboza5@gmail.com>
2022-06-07 12:53:51 -0300

    Exercicios

---
+++ modulo3-backend/b26-typescript/d2-tipagem-estatica-e-generics/class2.ts
@@ -0,0 +1,25 @@
+enum Color {
+  Black = "Pretos",
+  White = "Brancos",
+  Brown = "Castanhos",
+}
+
+class Dog {
+  name: string;
+  age: number;
+  color?: string;
+    constructor(name: string, age: number, color?: Color) {
+      this.name = name;
+      this.age = age;
+      this.color = color
+    }
+
+    apresentar(): void {
+      console.log(`Olá, eu sou o ${this.name}, tenho ${this.age} anos e meus pelos são ${this.color}`)
+    }
+}
+
+const dog1 = new Dog('Billy', 3, Color.Brown)
+const dog2 = new Dog('Felipe', 22)
+console.log(dog1);
+console.log(dog2);
