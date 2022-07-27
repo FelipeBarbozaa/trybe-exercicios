d7e77ede8d0e21c2f32b5f470d8aeb2fee697933
Felipe Barboza <felipebarboza5@gmail.com>
2022-07-27 14:41:57 -0300

    Atualizando

--- modulo3-backend/b30-mongo-node-poo/d1-mongo-msc/exercicios/src/middlewares/ErrorMiddleware.ts
+++ modulo3-backend/b30-mongo-node-poo/d1-mongo-msc/exercicios/src/middlewares/ErrorMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import ErrorHandler from '../utils/ErrorHandler';
 
-export default (error: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+export default (error: ErrorHandler, _req: Request, res: Response, _next: NextFunction) => {
   if (error.status) {
     return res.status(error.status).json({ message: error.message });
   }
